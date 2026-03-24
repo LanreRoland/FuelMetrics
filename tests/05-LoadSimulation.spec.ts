183d213fecb1736137f49a6b6aba59c7a3a6d581
LanreRoland <mikeandmike@mailinator.com>
2026-03-24 08:21:31 +0100

    Unskip LoadSimulation test by removing env flag check

--- tests/05-LoadSimulation.spec.ts
+++ tests/05-LoadSimulation.spec.ts
@@ -4,10 +4,7 @@ import { ensureAuthenticated, priceManagementLink } from './helpers/epump';
 // This test suite creates 20 individual test cases.
 // When run with 20 workers, they will execute concurrently.
 test.describe('Epump Concurrent Login Simulation', () => {
-  test.skip(
-    !process.env.EPUMP_RUN_LOAD_TESTS,
-    'Load simulation is opt-in. Set EPUMP_RUN_LOAD_TESTS=1 to enable it.',
-  );
+
   // We specify parallel mode explicitly for this suite
   test.describe.configure({ mode: 'parallel' });
   test.setTimeout(210000);
